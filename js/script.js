f3986ec20382c2a3f6bd1fda155143f654e6e1f6
João Henrique Ferreira <157930541+J-Ferre1ra@users.noreply.github.com>
2025-01-07 10:28:02 -0300

    Finalização da parte dinâmica

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 const btnSwitch = document.querySelector('.toggle-input')
+let title = document.querySelector('h1')
+let subtitle = document.querySelector('h2')
+let footerP = document.querySelector('p')
 
 btnSwitch.addEventListener('click', ()=>{
     const body = document.querySelector('body')
     if(btnSwitch.checked){
-        body.style.backgroundColor = '#191818'
+        body.style.backgroundColor = '#2e2d2d'
+        title.style.color = '#ffffff'
+        subtitle.style.color = '#ffffff'
+        footerP.style.color = '#ffffff'
     }else{
         body.style.backgroundColor = '#e4e4e4'
+        title.style.color = '#000000'
+        subtitle.style.color = '#000000'
+        footerP.style.color = '#000000'
     }
 })
